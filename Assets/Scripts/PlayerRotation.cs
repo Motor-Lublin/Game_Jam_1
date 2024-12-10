d7637a3f6a05363c9b74bd005155553d37792375
mn58835 <mn58835@tutanota.com>
2024-12-10 03:09:00 +0100

    Poprawa kamery i smoothing obracania

--- Assets/Scripts/PlayerRotation.cs
+++ Assets/Scripts/PlayerRotation.cs
@@ -4,8 +4,16 @@ using System.Collections.Generic;
 
 public class PlayerRotation : MonoBehaviour
 {
- public float rotationSpeed = 25f; // Speed of rotation
+ public float rotationSpeed = 0.07f; // Speed of rotation
  public LayerMask groundLayer;   // Layer to detect the ground or target plane
+ private float lastRotationUpdate;
+ [SerializeField] private float  rotationUpdateInterveal = 0.09f;
+ [SerializeField] private float  closeProximityRadius = 5f;
+ public float neutralFacingAngle = 90f; 
+ private Vector3 lastValidDirection;  
+ private Vector3 lastTargetPosition;  
+ private Vector3 direction;
+
 
     void Start(){
 
@@ -26,14 +34,38 @@ public class PlayerRotation : MonoBehaviour
         {
             Vector3 targetPosition = hitInfo.point;
 
+            float distance = Vector3.Distance(transform.position, targetPosition);
+            
+            if (Vector3.Distance(lastTargetPosition, targetPosition) < 0.2f) 
+        {
+            return; 
+        }
+
+            lastTargetPosition = targetPosition;
+
+
+
+            if (distance < closeProximityRadius)
+            {
+                Quaternion neutralRotation = Quaternion.Euler(0, neutralFacingAngle, 0);
+                transform.rotation = Quaternion.Slerp(transform.rotation, neutralRotation, rotationSpeed * Time.deltaTime);
+                return;
+            }
+
             Vector3 direction = (targetPosition - transform.position).normalized;
 
             direction.y = 0;
+
+            lastValidDirection = direction;
 
             Quaternion targetRotation = Quaternion.LookRotation(direction);
 
             transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
         }
+        else if (lastValidDirection != Vector3.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(lastValidDirection);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        }
     }
-
 }
